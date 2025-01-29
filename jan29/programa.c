886fb221e9868a10acf4201c0cd4e1a5a2729e55
gaelsolism <cg.solismercado@ugto.mx>
2025-01-29 11:30:01 -0600

    mi primera actualizacion

---
+++ jan29/programa.c
@@ -0,0 +1,3 @@
+/*
+esto  es un comentario, me sirve para escribir nformacion relevante del programa que estoy haciendo, por ejemplo:
+este programara imprimira la tipica frase "hola mundo" a la pantalla */
