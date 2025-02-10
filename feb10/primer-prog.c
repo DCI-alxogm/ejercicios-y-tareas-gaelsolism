2c5e12c4d57bd704755350dfdf0b056acaef74e8
gaelsolism <cg.solismercado@ugto.mx>
2025-02-10 11:28:41 -0600

    primer programa gael solis

---
+++ feb10/primer-prog.c
@@ -0,0 +1,10 @@
+/* creado el lunes 10 de febrero del 2025*/
+#include<stdio.h>
+int main ()
+{
+printf ("hola gael \n");
+printf ("Este es mi primer programa del curso \n");
+
+//termine de escribir el programa
+return 0;
+}
