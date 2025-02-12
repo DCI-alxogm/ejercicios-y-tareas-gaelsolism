6a4a7a29e5436a82313f2fd6fa44d8a26e9270da
gaelsolism <cg.solismercado@ugto.mx>
2025-02-12 11:54:35 -0600

    printf("...%s,:::%f...%i",var1,var2,var3);

--- 12Feb/segundo_prog.c
+++ 12Feb/segundo_prog.c
@@ -1,9 +1,9 @@
 /******************************************************************************
 
-Cristian Gael solis mercado
+Cristian Gael solis mercado.
 Ing.Química Sustentable
 Hoy es Miercoles 12 de Febrero del 2025.
-Segundo programa, variables 
+Segundo programa, variables.
 *******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,12 +17,11 @@ int main()
 scanf("%s",nombre);
 printf("hola %s, este es el segundo programa del curso PB 2025 \n",nombre);
 
-    printf("Cuál es tu edad ¿?\n");
+    printf("Cual es tu edad ¿?\n");
     scanf("%i",&edad);
-    printf("Qué temperatura marcó el termómetro la ultima vez que fuiste al supermercado?\n");
+    printf("Qué temperatura marcó el termometro la ultima vez que fuiste al supermercado?\n");
     scanf("%f",&temperatura);
 
-printf("tu edad es %i años \n",edad);
-printf("tu ultimo registro de temperatura fué %.f °C",temperatura);
+printf("tu nombre es %s, tienes %i años, y tu ultimo registro de temperatura fue de %f",nombre,edad,temperatura);
 exit(0);
 }
