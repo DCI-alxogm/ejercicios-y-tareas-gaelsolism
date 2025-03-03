d85985d9b04f5e3a13405400be743ef73d160fac
gaelsolism <cg.solismercado@ugto.mx>
2025-03-03 11:59:32 -0600

    coordenadas y temperatura en u solo programa

---
+++ marzo3/programacombinado.c
@@ -0,0 +1,27 @@
+//cristian Gael Solis Mercado
+//ingenieria quimica sustentable
+//hoy es 3 de marxo del 2025;
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+int main()
+{
+    //para coordenadas
+    float x,y,z;
+//para esfericas
+float resf,thetaesf, phi
+//parta cilindricas 
+float thetacil, rcil, 
+//para temperatura
+float tk,tc;
+//opciones
+char opcion[15];
+char repetir[5];
+printf("seleccione que es lo que desa hacer usted \n");
+printf("¿coordenadas esferiucas- cilindricas o conversion de temperatura?\n");
+scanf("%s",opcion);
+    
+    
+    
+    
+    
